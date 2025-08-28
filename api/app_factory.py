f83a98322934de7a86f007cb55b6adce831dc105
zzl <253955284@qq.com>
2025-08-28 16:33:17 +0800

    Revert "添加API Key认证方式，更新召回测试API接口"
    This reverts commit 68518706797395d9c06e81a878d84e3886c49719.

--- api/app_factory.py
+++ api/app_factory.py
@@ -15,9 +15,7 @@ def create_flask_app_with_configs() -> DifyApp:
     with configs loaded from .env file
     """
     dify_app = DifyApp(__name__)
-    dify_app.config.from_mapping(dify_config.model_dump())   
-    # 设置 JSON 编码，让中文正常显示而不是 Unicode 编码
-    dify_app.config['JSON_AS_ASCII'] = False
+    dify_app.config.from_mapping(dify_config.model_dump())
 
     # add before request hook
     @dify_app.before_request
